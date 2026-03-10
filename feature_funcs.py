c1e1a30731a465d07a147e255dd2b5ed908f202f
trucar00 <98335830+trucar00@users.noreply.github.com>
2026-03-10 17:18:10 +0100

    Building feats

--- feature_funcs.py
+++ feature_funcs.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy  as np
 
-def haversine(lat1, lon1, lat2, lon2, dt):
+def haversine(lat1, lon1, lat2, lon2):
     R = 6371000 # Radius of the earth in meters
 
-    lat1 = np.radians(lat1)
-    lon1 = np.radians(lon1)
-    lat2 = np.radians(lat2)
-    lon2 = np.radians(lon2)
+    lat1 = np.radians(np.asarray(lat1, dtype=float))
+    lon1 = np.radians(np.asarray(lon1, dtype=float))
+    lat2 = np.radians(np.asarray(lat2, dtype=float))
+    lon2 = np.radians(np.asarray(lon2, dtype=float))
 
     dlat = lat2 - lat1
     dlon = lon2 - lon1
@@ -20,9 +20,12 @@ def haversine(lat1, lon1, lat2, lon2, dt):
     c = 2 * np.arcsin(np.sqrt(a))
 
     dist = R * c
-    speed = (dist/dt) * 1.94384 # Convert m/s to knots
+    #speed = (dist/dt) * 1.94384 # Convert m/s to knots
 
-    return dist, speed
+    return dist #, speed
+
+def speed(dist, dt):
+    return (dist/dt) * 1.94384
 
 def angle_wrap(a):
     return (a + 180) % 360 - 180
@@ -39,5 +42,13 @@ def dcourse(cog1, cog2, dt):
 
 if __name__ == "__main__":
     print("yo")
-    #df["del_cog"] = df["del_cog"].apply(angle_wrap)
+    df = pd.DataFrame(columns=["cog", "del_cog", "pos"])
+    df["cog"] = [360, 10, 360, 20, 40, 358]
+    df["del_cog"] = df["cog"].diff()
+    df["del_cog"] = angle_wrap(df["del_cog"])
+    #print(df.head())
+
+    df["pos"] = [1, 2, 3, 4, 5, 6]
+    print(df["pos"][:-1])
+    print(df["pos"][1:])
     
--- sliding_window.py
+++ sliding_window.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import feature_funcs as ff
+import numpy as np
 
 # READY FOR CREATING SEGMENTS
 
-df = pd.read_csv("Data/gfw/gfw_trawlers_cog.csv")
+df = pd.read_csv("Data/gfw_trawlers_cog.csv")
 df["datetime"] = pd.to_datetime(df["datetime"])
 df["traj_num"] = df["trajectory_id"].astype(str).str.rsplit("-", n=1).str[-1].astype(int)
 
@@ -17,31 +18,59 @@ fish_segs = 0
 steam_segs = 0
 no_label = 0
 
-feature_df = pd.DataFrame(columns=["mmsi", "datetime", "dist_to_prev", "jerk", "speed",])
-#feature_df
+all_feature_dfs = []
 
 for traj, d in df.groupby("trajectory_id", sort=False):
     d = d.sort_values("datetime").reset_index(drop=True)
     start_idx = 0
     end_idx = nr_points
 
-
     while end_idx <= len(d):
         segment = d.iloc[start_idx:end_idx]
         labeled = segment[segment["is_fishing"] != -1]
         n_labeled = len(labeled)
+        
 
         if n_labeled < min_labeled:
             no_label += 1
         else:
+            feature_df = pd.DataFrame() # must concat all segment dfs
+            feature_df["mmsi"] = segment["mmsi"]
+            feature_df["trajectory_id"] = segment["trajectory_id"]
+            feature_df["datetime"] = segment["datetime"]
+            feature_df["lon"] = segment["lon"]
+            feature_df["lat"] = segment["lat"]
+            feature_df["cog_interp"] = segment["cog_interp"]
+            feature_df["del_cog"] = segment["cog_interp"].diff().apply(ff.angle_wrap)
+            
+            lon1, lon2 = segment["lon"][:-1], segment["lon"][1:]
+            lat1, lat2 = segment["lat"][:-1], segment["lat"][1:]
+            dist = ff.haversine(lat1, lon1, lat2, lon2)
+            dist = np.insert(dist, 0, np.nan)
+            feature_df["dist_to_prev"] = dist
+
+            dt = feature_df["datetime"].diff().dt.total_seconds()
+            speed = ff.speed(dist, dt)
+            feature_df["speed"] = speed
+            #feature_df["accel"] = ff.accel() # we'll get two nans?
+            #feature_df["jerk"]
+            
             vote = (labeled["is_fishing"] > 0.5).sum()
             if vote >= (n_labeled/2):
                 fish_segs += 1
+                feature_df["fishing"] = 1
             else:
                 steam_segs += 1
+                feature_df["fishing"] = 0
+            
+            all_feature_dfs.append(feature_df)
 
         start_idx += slide
         end_idx += slide
 
+features_all = pd.concat(all_feature_dfs)
+print(features_all.shape) # fishing + steaming segments * 11 
 
 print("Fishing segments: ", fish_segs, " Steaming segs: ", steam_segs, " No label: ", no_label)
+features_all = features_all.drop(columns=["mmsi", "trajectory_id", "datetime"])
+print(features_all.head())
